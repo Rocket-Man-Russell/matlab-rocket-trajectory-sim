d6f5b7e57525a47fed7d0e678ac8f9da229aa805
F. Russell <Rocket-Man-Russell@users.noreply.github.com>
2017-05-23 09:44:36 +0100

    Add files via upload

--- liftCoef.m
+++ liftCoef.m
@@ -3,18 +3,22 @@ function [sim_liftCoef] = liftCoef(sim_angleOfAttack)
             sim_liftCoef = 1.1;
             
 	elseif  (abs(sim_angleOfAttack*(180/pi)) <= 20)
-            sim_liftCoef = (-1.824*pi*sim_angleOfAttack + 2.583);
+            sim_liftCoef = (-1.824*pi*abs(sim_angleOfAttack) + 2.583);
             
     elseif  (abs(sim_angleOfAttack*(180/pi)) <= 36)
-            sim_liftCoef = (0.472*pi*sim_angleOfAttack + 0.069);
+            sim_liftCoef = (0.472*pi*abs(sim_angleOfAttack) + 0.069);
             
 	elseif  (abs(sim_angleOfAttack*(180/pi)) <= 55)
             sim_liftCoef = 1.0;
             
 	elseif  (abs(sim_angleOfAttack*(180/pi)) <= 125) % <= 90)
-            sim_liftCoef = (-0.474*pi*sim_angleOfAttack + 2.445);
+            sim_liftCoef = (-0.474*pi*abs(sim_angleOfAttack) + 2.445);
     else
             sim_liftCoef = 0;
 	end
+    
+        if (sim_angleOfAttack*(180/pi)) < 0
+            sim_liftCoef = -1*sim_liftCoef;
+        end
 end
 % sim_liftCoef(simCounter) = liftCoef(sim_angleOfAttack(simCounter))
